ad2e9e941a296a0fafe7baae84eacb82bcb1f107
Sercan Akmaz <sercan.akmaz@hepsiburada.com>
2019-07-09 09:10:09 +0300

    added

---
+++ JWTAuthenticationAuthorization/Startup.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthenticationAuthorization
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(config =>
+            {
+//                config.Filters.Add(typeof(ClaimRequirementFilter));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddAuthentication(options =>
+                    {
+                        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    }
+                )
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(
+                                "uzun ince bir yoldayım şarkısını buradan tüm sevdiklerime hediye etmek istiyorum mümkün müdür acaba?"))
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = ctx =>
+                        {
+                           var userId = ctx.Principal.Claims.Where(p => p.ValueType == ClaimTypes.NameIdentifier);
+                            //Gerekirse burada gelen token içerisindeki çeşitli bilgilere göre doğrulam yapılabilir.
+                            return Task.CompletedTask;
+                        },
+                        OnAuthenticationFailed = ctx =>
+                        {
+                            Console.WriteLine("Exception:{0}", ctx.Exception.Message);
+                            return Task.CompletedTask;
+                        },
+                        OnChallenge = ctx =>
+                        {
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+            services.AddSingleton<IAuthorizationPolicyProvider, AuthorizationPolicyProvider>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+    
+    public class PermissionAuthorizeAttribute : AuthorizeAttribute
+    {
+        internal const string PolicyPrefix = "PERMISSION:";
+        /// <summary>
+        /// Creates a new instance of <see cref="AuthorizeAttribute"/> class.
+        /// </summary>
+        /// <param name="permissions">A list of permissions to authorize</param>
+        public PermissionAuthorizeAttribute(params string[] permissions)
+        {
+            Policy = $"{PolicyPrefix}{string.Join(",", permissions)}";
+        }
+    }
+    public class AuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
+    {
+        public AuthorizationPolicyProvider(IOptions<AuthorizationOptions> options)
+            : base(options)
+        {
+        }
+
+        public override Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+        {
+            if (!policyName.StartsWith(PermissionAuthorizeAttribute.PolicyPrefix, StringComparison.OrdinalIgnoreCase))
+            {
+                return base.GetPolicyAsync(policyName);
+            }
+
+            var permissionNames = policyName.Substring(PermissionAuthorizeAttribute.PolicyPrefix.Length).Split(',');
+
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireClaim(CustomClaimTypes.Permission, permissionNames)
+                .Build();
+
+            return Task.FromResult(policy);
+        }
+    }
+    public class CustomClaimTypes
+    {
+        public const string Permission = "projectname/permission";
+    }
+
+    public class CustomPermissions
+    {
+        public const string CanReadValues = "CanReadValues";
+        public const string CanWriteValues = "CanWriteValues";
+    }
+}
